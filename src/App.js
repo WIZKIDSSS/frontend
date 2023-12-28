cfd694b55387ea895ec1e842c75c134b8c69fa65
Keshav Jadhav <jadavkeshav2005@gmail.com>
2023-12-28 16:57:48 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
-import Login from './Component.js/Login';
+import Login from './Component/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
-  return (
+  return (<>
     <Login />
+
+
+    <ToastContainer position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored" />
+  </>
+
+
+
+
+    
   );
 }
 
